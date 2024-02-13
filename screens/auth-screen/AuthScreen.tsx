d8d330a24959305e8bc1933870379ad95a7a4867
Lucrada <iamdracula.dracula@gmail.com>
2024-02-13 13:00:50 +0530

    added credential validation

--- screens/auth-screen/AuthScreen.tsx
+++ screens/auth-screen/AuthScreen.tsx
@@ -1,36 +1,65 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, Alert } from 'react-native';
 import styles from './Style';
 import VerticalSpacer from '../../utils/VerticalSpacer';
 import { TextInput } from 'react-native-gesture-handler';
 
-const AuthScreen = (): React.JSX.Element => {
+const AuthScreen = ({ navigation }): React.JSX.Element => {
     const [loginView, setLoginView] = React.useState(true);
+
+    const [loginUsername, setLoginUsername] = React.useState('');
+    const [loginPassword, setLoginPassword] = React.useState('');
+    const [registerUsername, setRegisterUsername] = React.useState('');
+    const [registerPassword, setRegisterPassword] = React.useState('');
+    const [registerConfPassword, setRegisterConfPassword] = React.useState('');
+
+    const _validateCredentials = (username, password, confPass = null) => {
+        if (username.trim().length === 0) { return { success: false, message: 'Username cannot be empty' }; }
+        if (password.trim().length === 0) { return { success: false, message: 'Password cannot be empty' }; }
+        if (confPass != null && (confPass.trim().length === 0 || password.trim() !== confPass.trim())) { return { success: false, message: 'Passwords are not matching' }; }
+        return { success: true };
+    };
+
+    const handleLogin = () => {
+        let result = _validateCredentials(loginUsername, loginPassword);
+        if (!result.success) {
+            Alert.alert('Warning', result.message);
+            return;
+        }
+    };
+
+    const handleRegister = () => {
+        let result = _validateCredentials(registerUsername, registerPassword, registerConfPassword);
+        if (!result.success) {
+            Alert.alert('Warning', result.message);
+            return;
+        }
+    };
 
     return (
         <View style={styles.mainContainer}>
             {loginView ? <View style={styles.col}>
-                <Text style={styles.title}>TaskTracker</Text>
+                <Text style={styles.title}>TaskTracker Pro</Text>
                 <VerticalSpacer amount={40} />
-                <TextInput style={{ ...styles.textbox, ...styles.neumorphicContainer }} placeholder="Username" placeholderTextColor={'gray'} cursorColor={'gray'} />
+                <TextInput value={loginUsername} onChangeText={(text) => setLoginUsername(text)} style={{ ...styles.textbox, ...styles.neumorphicContainer }} placeholder="Username" placeholderTextColor={'gray'} cursorColor={'gray'} />
                 <VerticalSpacer amount={25} />
-                <TextInput style={{ ...styles.textbox, ...styles.neumorphicContainer }} placeholder="Password" placeholderTextColor={'gray'} secureTextEntry={true} cursorColor={'gray'} />
+                <TextInput value={loginPassword} onChangeText={(text) => setLoginPassword(text)} style={{ ...styles.textbox, ...styles.neumorphicContainer }} placeholder="Password" placeholderTextColor={'gray'} secureTextEntry={true} cursorColor={'gray'} />
                 <VerticalSpacer amount={40} />
-                <TouchableOpacity style={{ ...styles.loginButtonContainer, ...styles.neumorphicContainer }}><Text style={styles.loginButtonText}>Log in</Text></TouchableOpacity>
+                <TouchableOpacity onPress={handleLogin} style={{ ...styles.loginButtonContainer, ...styles.neumorphicContainer }}><Text style={styles.loginButtonText}>Log in</Text></TouchableOpacity>
                 <VerticalSpacer amount={25} />
                 <TouchableOpacity onPress={() => setLoginView(false)} style={{ ...styles.registerButtonContainer, ...styles.neumorphicContainer }}><Text style={styles.registerButtonText}>Do not have an account ? Register</Text></TouchableOpacity>
             </View> :
                 <View style={styles.col}>
-                    <Text style={styles.title}>TaskTracker</Text>
+                    <Text style={styles.title}>TaskTracker Pro</Text>
                     <VerticalSpacer amount={40} />
-                    <TextInput style={{ ...styles.textbox, ...styles.neumorphicContainer }} placeholder="Username" placeholderTextColor={'gray'} cursorColor={'gray'} />
+                    <TextInput value={registerUsername} onChangeText={(text) => setRegisterUsername(text)} style={{ ...styles.textbox, ...styles.neumorphicContainer }} placeholder="Username" placeholderTextColor={'gray'} cursorColor={'gray'} />
                     <VerticalSpacer amount={25} />
-                    <TextInput style={{ ...styles.textbox, ...styles.neumorphicContainer }} placeholder="Password" placeholderTextColor={'gray'} secureTextEntry={true} cursorColor={'gray'} />
+                    <TextInput value={registerPassword} onChangeText={(text) => setRegisterPassword(text)} style={{ ...styles.textbox, ...styles.neumorphicContainer }} placeholder="Password" placeholderTextColor={'gray'} secureTextEntry={true} cursorColor={'gray'} />
                     <VerticalSpacer amount={25} />
-                    <TextInput style={{ ...styles.textbox, ...styles.neumorphicContainer }} placeholder="Confirm Password" placeholderTextColor={'gray'} secureTextEntry={true} cursorColor={'gray'} />
+                    <TextInput value={registerConfPassword} onChangeText={(text) => setRegisterConfPassword(text)} style={{ ...styles.textbox, ...styles.neumorphicContainer }} placeholder="Confirm Password" placeholderTextColor={'gray'} secureTextEntry={true} cursorColor={'gray'} />
                     <VerticalSpacer amount={40} />
-                    <TouchableOpacity style={{ ...styles.loginButtonContainer, ...styles.neumorphicContainer }}><Text style={styles.loginButtonText}>Register</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={handleRegister} style={{ ...styles.loginButtonContainer, ...styles.neumorphicContainer }}><Text style={styles.loginButtonText}>Register</Text></TouchableOpacity>
                     <VerticalSpacer amount={25} />
                     <TouchableOpacity onPress={() => setLoginView(true)} style={{ ...styles.registerButtonContainer, ...styles.neumorphicContainer }}><Text style={styles.registerButtonText}>Already have an account ? Login</Text></TouchableOpacity>
                 </View>}
